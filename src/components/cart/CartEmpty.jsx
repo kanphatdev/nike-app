f937f1b2e2a479984d683922d4a7ae6a09bc5783
kanphatratrazer <kanphatratrazer28@gmail.com>
2024-02-01 15:07:07 +0700

    CartEmpty component modify

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -2,7 +2,7 @@
 import emptybag from '../../assets/emptybag.png';
 import { ArrowLeftIcon  } from '@heroicons/react/24/outline';
 
-const CartEmpty = () => {
+const CartEmpty = ({ onCartToggle }) => {
   return (
    <>
       <div className='flex items-center justify-center flex-col h-screen px-11 text-center gap-7'>
